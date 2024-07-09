b6a8181c24fb2851f8bab06cbd5623a3d14b9b11
testervdk <margarita.se@bk.ru>
2024-07-09 17:27:09 +1000

    fix: переписала метод authorizationResponse, убрала ненужные методы

--- src/main/java/services/Auth.java
+++ src/main/java/services/Auth.java
@@ -2,44 +2,21 @@ package services;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import static io.restassured.RestAssured.given;
 
 public class Auth {
-    private static final String BASE_URI = "https://chudodey.com";
+    private static final String BASE_URL = "https://chudodey.com";
 
     public Auth() {
-        RestAssured.baseURI = BASE_URI;
+        RestAssured.baseURI = BASE_URL;
     }
 
     public Response authorizationResponse(String phone, String password) {
         return given()
-                .contentType("application/json")
-                .formParam("telephone", phone)
-                .formParam("password", password)
+                .formParam("AccountsDate[telephone]", phone)
+                .formParam("AccountsDate[password]", password)
                 .when()
-                .post("/accounts/login")
-                .then()
-                .extract().response();
-    }
-
-    public String contentType(String phone, String password) {
-        return authorizationResponse(phone, password).contentType();
-    }
-
-    public boolean isUserDataWrong(String phone, String password, String message) {
-        if(contentType(phone, password).contains("text/html")) {
-            String htmlBody = authorizationResponse(phone, password).getBody().asString();
-
-            Document doc = Jsoup.parse(htmlBody);
-            Element errorElement = doc.selectFirst("div.bottom-error");
-
-            return errorElement != null && errorElement.text().equals(message);
-        }
-
-        return false;
+                .post("/accounts/authorization");
     }
 }
