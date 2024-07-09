af43934357fdf836799497aae48656b62e645d0e
testervdk <margarita.se@bk.ru>
2024-07-09 17:27:41 +1000

    fix: внесла правки в AuthAPITest

--- src/main/java/tests/api/AuthAPITest.java
+++ src/main/java/tests/api/AuthAPITest.java
@@ -6,24 +6,22 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import services.Auth;
 
-public class AuthorizationTest {
+public class AuthAPITest {
     private Auth authService;
 
     String userPhoneNumber = "+7 000 000 00 00";
     String userPassword = "123456";
-    String expectedErrorMessage = "Неверная комбинация логина и пароля";
 
     @BeforeTest
     public void setUp() {
         authService = new Auth();
     }
 
-
     @Test
     public void invalidAuthorization() {
         Response response = (authService.authorizationResponse(userPhoneNumber, userPassword));
 
         Assert.assertEquals(200, response.getStatusCode());
-        Assert.assertTrue(authService.isUserDataWrong(userPhoneNumber, userPassword, expectedErrorMessage));
+        Assert.assertEquals("application/json", response.getContentType());
     }
 }
