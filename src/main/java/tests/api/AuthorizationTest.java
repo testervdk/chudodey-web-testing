5e7f03d26f88e60f5701fa020aaeb2427d6f0025
testervdk <margarita.se@bk.ru>
2024-07-09 11:41:43 +1000

    fix: изменила структуру теста invalidAuthorization

--- src/main/java/tests/api/AuthorizationTest.java
+++ src/main/java/tests/api/AuthorizationTest.java
@@ -2,23 +2,28 @@ package tests.api;
 
 import io.restassured.response.Response;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import services.Auth;
 
 public class AuthorizationTest {
+    private Auth authService;
+
+    String userPhoneNumber = "+7 000 000 00 00";
+    String userPassword = "123456";
+    String expectedErrorMessage = "Неверная комбинация логина и пароля";
+
+    @BeforeTest
+    public void setUp() {
+        authService = new Auth();
+    }
+
 
     @Test
     public void invalidAuthorization() {
-        Auth authService = new Auth();
-
-        String userPhoneNumber = "+7 000 000 00 00";
-        String userPassword = "123456";
-        String expectedErrorMessage = "Неверная комбинация логина и пароля";
-
         Response response = (authService.authorizationResponse(userPhoneNumber, userPassword));
 
         Assert.assertEquals(200, response.getStatusCode());
         Assert.assertTrue(authService.isUserDataWrong(userPhoneNumber, userPassword, expectedErrorMessage));
     }
-
 }
